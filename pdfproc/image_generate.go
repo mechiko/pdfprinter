20a2e2b85506c6fe735876348efe5e8b04f7fdba
mechiko <kbprime@gmail.com>
2025-11-26 15:25:36 +0300

    fix

--- pdfproc/image_generate.go
+++ pdfproc/image_generate.go
@@ -7,6 +7,7 @@ import (
 	"image/draw"
 	"image/jpeg"
 	"image/png"
+	"os"
 
 	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/datamatrix"
@@ -49,46 +50,51 @@ func (p *pdfProc) dmImg(code string) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("%w", err)
 	}
-	// err = p.reader(code, scaledCheck)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("%w", err)
-	// }
 	var b, bc bytes.Buffer
-	// jpeg.Encode(&b, scaled, nil)
 	err = png.Encode(&b, scaled)
 	if err != nil {
 		return nil, fmt.Errorf("%w", err)
 	}
 	err = png.Encode(&bc, scaledCheck)
 	if err != nil {
-		return nil, fmt.Errorf("%w", err)
+		return nil, fmt.Errorf("encode 8-bit png: %w", err)
 	}
-	// if err := os.WriteFile("file.png", b.Bytes(), 0666); err != nil {
-	// 	return nil, fmt.Errorf("%w", err)
-	// }
-	// if err := os.WriteFile("check.png", bc.Bytes(), 0666); err != nil {
-	// 	return nil, fmt.Errorf("%w", err)
-	// }
+	if p.DebugMode() {
+		if err := os.WriteFile("file.png", b.Bytes(), 0666); err != nil {
+			return nil, fmt.Errorf("%w", err)
+		}
+		if err := os.WriteFile("check.png", bc.Bytes(), 0666); err != nil {
+			return nil, fmt.Errorf("%w", err)
+		}
+	}
 	err = p.reader(code, bc)
 	if err != nil {
 		return nil, fmt.Errorf("%w", err)
 	}
-	return forceTo8BitPNG(b.Bytes()), nil
+	out, err := forceTo8BitPNG(b.Bytes())
+	if err != nil {
+		return nil, fmt.Errorf("normalize datamatrix png: %w", err)
+	}
+	return out, nil
 }
 
-func forceTo8BitPNG(data []byte) []byte {
-	src, _ := png.Decode(bytes.NewReader(data))
+func forceTo8BitPNG(data []byte) ([]byte, error) {
+	src, err := png.Decode(bytes.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("decode png: %w", err)
+	}
 	b := src.Bounds()
 	dst := image.NewNRGBA(b)
 	draw.Draw(dst, b, src, b.Min, draw.Src)
 	buf := new(bytes.Buffer)
-	png.Encode(buf, dst)
-	return buf.Bytes()
+	if err := png.Encode(buf, dst); err != nil {
+		return nil, fmt.Errorf("encode 8-bit png: %w", err)
+	}
+	return buf.Bytes(), nil
 }
 
 // генератор PNG
 func dmImgJpg(code string, size int) ([]byte, error) {
-	// fmt.Printf("KM len %d\n", len(code))
 	bcImg, err := datamatrix.Encode(code)
 	if err != nil {
 		return nil, fmt.Errorf("%w", err)
--- pdfproc/reader_check.go
+++ pdfproc/reader_check.go
@@ -15,25 +15,25 @@ import (
 func (p *pdfProc) reader(code string, png bytes.Buffer) error {
 	img, _, err := image.Decode(&png)
 	if err != nil {
-		return fmt.Errorf("Unable to convert to DataMatrix-code to bitmap: %s", err)
+		return fmt.Errorf("Unable to decode to DataMatrix code to bitmap: %s", err)
 	}
 	// prepare BinaryBitmap
 	bmp, err := dmxing.NewBinaryBitmapFromImage(img)
 	if err != nil {
-		return fmt.Errorf("Unable to convert to DataMatrix-code to bitmap: %s", err)
+		return fmt.Errorf("Unable to decode to DataMatrix code to bitmap: %s", err)
 	}
 
 	// decode image
 	datamatrixReader := datamatrix.NewDataMatrixReader()
 	result, err := datamatrixReader.Decode(bmp, nil)
 	if err != nil {
-		return fmt.Errorf("Unable to convert to DataMatrix-code to bitmap: %s", err)
+		return fmt.Errorf("Unable to decode to DataMatrix code to bitmap: %s", err)
 	}
 	hx := strconv.Quote(result.GetText())
-	cisQuted := strconv.Quote(code)
-	if hx == cisQuted {
+	codeQuoted := strconv.Quote(code)
+	if hx == codeQuoted {
 		return nil
 	}
-	return fmt.Errorf("code [%s] not equal png decode [%s]", cisQuted, hx)
+	return fmt.Errorf("code [%s] not equal png decode [%s]", codeQuoted, hx)
 
 }
